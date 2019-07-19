a32ba84b37eed0cacb0d6776fbe69b709835e42c
rajendram <rajendra@mitrmedia.com>
2019-07-19 17:09:58 +0530

    script added

---
+++ svnCreationPowershell.ps1
@@ -0,0 +1,72 @@
+﻿# Create SVN Repository
+
+# change to svn repository directory
+cd E:\Repositories
+$repoPath = Get-Location
+
+Write-Host "Directory is change to $(Get-Location)"
+
+Write-Host
+
+# Ask user to input new repository name
+$newSVN = Read-Host "Please enter name of new SVN"
+$pattern = "^[a-zA-Z0-9_-]*$"
+
+# Test-Path $repoPath"\$newSVN"
+
+if (Test-Path $repoPath"\$newSVN")
+    {
+        Write-Host "SVN with name $newSVN already exists in $repoPath directory, Shutting down script!!!"
+        sleep 1
+        break
+    }
+
+if ($newSVN -match $pattern)
+    {
+        Write-Host "Please wait!!!"
+        sleep 2
+        $choice = Read-Host "Do you really want to create $newSVN SVN, enter Y or N?"
+        if ($choice -eq 'n'.ToLower())
+        {
+            Write-Host "Exiting Script"
+            break
+        }
+        elseif ($choice -eq 'y'.ToLower())
+        {
+            Write-Host "Creating SVN"
+            sleep 1
+
+        }
+        else
+        {
+            break
+        }
+
+    }
+
+# change directory to svn acl
+sleep 1
+cd E:\svn_repository_acls
+Write-Host "Directory changed to $(Get-Location) "
+
+Write-Verbose -Message "Creating access file" -Verbose
+sleep 1
+$svnACl = $newSVN + '-acl.conf'
+
+# Create file and add content to file
+Add-Content $svnACl -Value "[$newSVN`:/]`n@allusers = rw`n@admin = rw`n`n`n[groups]`nadmin = sujitn`nallusers = rajendram,prathameshs,sumitw"
+
+# change directory to httpd conf
+cd "C:\Program Files (x86)\Apache Software Foundation\Apache2.2\conf"
+Write-Verbose "Appending httpd.conf" -Verbose
+sleep 1
+Add-Content .\httpd.conf -Value "`n<Location /$newSVN>`n  DAV svn`n  SVNPath 'd:\\repositories\\$newSVN'`n  AuthzSVNAccessFile d:\svn_repository_acls\\$newSVN-acl.conf`n  AuthType Basic`n  AuthBasicProvider ldap`n  AuthzLDAPAuthoritative on`n  AuthName 'Subversion Repository'`n  AuthLDAPBindPassword mitr@svn`n  AuthLDAPURL 'ldap://172.19.10.10:3268/dc=mitrmum,dc=com?sAMAccountName?sub?(objectClass=*)'`n  Require valid-user`n</Location>"
+
+Write-Host "$newSVN SVN is created." -ForegroundColor Green
+
+# Restart apache service
+Write-Host "Restarting apache please wait!!!!!" -ForegroundColor DarkYellow
+sleep 2
+cd ..\bin
+.\httpd.exe -l restart
+
---
+++ svnCreationPython.py
@@ -0,0 +1,77 @@
+﻿import os, re
+from time import sleep
+
+# change to svn repository directory
+os.chdir(r'D:\repositories')
+print("Directory is change to " + os.getcwd())
+
+# Ask user to input new repository name
+newSVN = input("Please enter name of new SVN: ")
+
+if os.path.exists(newSVN):
+    print("SVN with name {} already exists in {} directory, Shutting script down!!!.".format(newSVN, os.getcwd()))
+    exit()
+if re.findall("^[a-zA-Z0-9_-]*$", newSVN):
+    print("Please wait!!!!")
+    sleep(2)
+    choice = input("Do you really want to create {}, enter {} or {}? ".format(newSVN, 'Y', 'N')).lower()
+    if choice == 'n':
+        quit()
+    else:
+        os.system('svnadmin create ' + newSVN)
+        sleep(2)
+else:
+    print("Please enter a valid SVN name eg:- P100241_IKEA_35_Language ")
+    exit()
+
+# change directory to svn acl
+print()
+os.chdir(r'D:\svn_repository_acls')
+print("Directory now changed to " + os.getcwd())
+print("Creating access file")
+sleep(2)
+
+# now create a conf file
+svnACl = newSVN + '-acl.conf'
+file = open(svnACl,'w')
+text_in_file = ['[', newSVN, ':/]', '\n', '@allusers = rw', '\n', '@admin = rw', '\n'*4, '[groups]', '\n', 'admin = sujitn', '\n', 'allusers = rajendram,prathameshs,sumitw,vickym']
+file.writelines(text_in_file)
+file.close()
+
+# change directory to httpd conf
+os.chdir(r'C:\Program Files (x86)\Apache Software Foundation\Apache2.2\conf')
+print()
+print("Appending httpd.conf")
+sleep(1)
+
+text_to_append = """\n
+<Location /P100241_IKEA_35_Language>
+  DAV svn
+  SVNPath "d:\\repositories\\P100241_IKEA_35_Language
+  AuthzSVNAccessFile d:\svn_repository_acls\\P100241_IKEA_35_Language-acl.conf
+  AuthType Basic
+  AuthBasicProvider ldap
+  AuthzLDAPAuthoritative on
+  AuthName "Subversion Repository"
+  AuthLDAPBindDN "SVN-Admin@mitrmum.com"
+  AuthLDAPBindPassword mitr@svn
+  AuthLDAPURL "ldap://172.19.10.10:3268/dc=mitrmum,dc=com?sAMAccountName?sub?(objectClass=*)"
+  Require valid-user
+</Location>""".replace('P100241_IKEA_35_Language',newSVN)
+
+with open("httpd.conf", 'a') as myfile:
+    myfile.write(text_to_append)
+
+print('=' * 50)
+print(newSVN + " repository is created.")
+print('=' * 50)
+
+# Restart apache service
+print("Restarting apache please wait!!!!!")
+sleep(2)
+os.chdir(r'C:\Program Files (x86)\Apache Software Foundation\Apache2.2\bin')
+cmd = os.system('httpd.exe -k restart')
+if cmd == 0:
+    print('Apache restart successfully')
+else:
+    print('Something went wrong please check manually')
